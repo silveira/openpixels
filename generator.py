48e6bf046639da91b44062c44d19cd11523af2c8
Shawn Wilkinson <me@super3.org>
2012-12-19 06:24:34 -0500

    Renamed More Layers and Script Is Done

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 # Imports
 import os
+import argparse
 from PIL import Image
 from random import choice
 
@@ -14,22 +15,76 @@ def dirFiles(dname):
 	return fileList
 
 def compose(layerList):
+	"""Returns layered image from a passed list image paths."""
 	img = None
 	for layer in layerList:
+		# Set first image to background
 		if img == None:
 			img = Image.open(layer)
 			continue
+		# Ignore skipped layer
+		elif layer == None:
+			continue
+		# Add all aditional images to that background
 		layerObj = Image.open(layer)
 		img.paste(layerObj, (0,0), mask=layerObj)
 	return img
 
-# Main
-for i in range(50):
+def generate(body = True, cloths = True, eyes = True, hair = True, hat = True, acc = True, shadow = False, shoes = False):
+	"""Generates a character based on the passed arguments."""
+	
+	# Get paths to all layer images
+	body_files = dirFiles("layers/body")
+	cloths_files = dirFiles("layers/cloths")
+	eyes_files = dirFiles("layers/eyes")
+	hair_files = dirFiles("layers/hair")
+	hat_files = dirFiles("layers/hat")
+	acc_files = dirFiles("layers/accessories")
+
+	# Generate layers
 	layers = []
-	layers.append( choice(dirFiles("layers/body")) )
-	layers.append( choice(dirFiles("layers/cloths")) )
-	layers.append( choice(dirFiles("layers/eyes")) )
-	layers.append( choice(dirFiles("layers/hair")) )
-	layers.append( choice(dirFiles("layers/hat")) )
-	layers.append( choice(dirFiles("layers/misc")) )
-	compose(layers).save("export/export"+str(i)+".png", "PNG")
+	if shadow: layers.append( "layers/shadow.png" )
+	layers.append( file_in_dir(body_files, body) )
+	layers.append( file_in_dir(cloths_files, cloths) )
+	layers.append( file_in_dir(eyes_files, eyes) )
+	layers.append( file_in_dir(hair_files, hair) )
+	layers.append( file_in_dir(cloths_files, hat) )
+	layers.append( file_in_dir(acc_files, acc) )
+	if shoes: layers.append( "layers/shoes.png" )
+
+	# Compose and return
+	return compose(layers)
+
+def file_in_dir(paths, search_str):
+	if not search_str: return None
+	elif str(search_str) == "Any":
+	 	return choice(paths)
+	for path in paths:
+		if path.find(str(search_str)) != -1: return path
+	return choice(paths)
+
+
+# Main Command Line
+parser = argparse.ArgumentParser(description='Generate character assets.')
+parser.add_argument("number", type=int, help="number of characters to generate")
+
+# Optional Arguments
+parser.add_argument("-body", help="show body", default=True)
+parser.add_argument("-cloth", help="show cloths", default=False)
+parser.add_argument("-eye", help="show eyes", default=False)
+parser.add_argument("-hair", help="show hair", default=False)
+parser.add_argument("-hat", help="show hats", default=False)
+parser.add_argument("-acc", help="show accessories", default=False)
+
+# Optional Arguments
+parser.add_argument("-shadow", help="show drop shadown", action="store_true", default=False)
+parser.add_argument("-shoe", help="show shoes", action="store_true", default=False)
+
+# Parse Arguments
+args = parser.parse_args()
+
+# Main
+for i in range(args.number):
+	char = generate(args.body, args.cloth, args.eye, args.hair, args.hat, args.acc, args.shadow, args.shoe)
+	char.save("export/export"+str(i)+".png", "PNG")
+	print("Saving '"+"export/export"+str(i)+".png"+"'")
