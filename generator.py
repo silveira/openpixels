9a8187117bb4178317de2f67fd4f82a3713e8f72
Shawn Wilkinson <me@super3.org>
2012-12-19 04:50:04 -0500

    Workspace Cleanup

---
+++ generator.py
@@ -0,0 +1,33 @@
+# Imports
+import os
+from PIL import Image
+from random import choice
+
+# Functions
+def dirFiles(dname):
+	"""Return a list of files from a passed directory path."""
+	fileList = []
+	for r,d,f in os.walk(dname):
+		for afile in f:
+			afile = str(os.path.join(r,afile)).replace("\\", "/")
+			fileList.append(afile)
+	return fileList
+
+def compose(layerList):
+	img = None
+	for layer in layerList:
+		if img == None:
+			img = Image.open(layer)
+			continue
+		layerObj = Image.open(layer)
+		img.paste(layerObj, (0,0), mask=layerObj)
+	return img
+
+# Main
+for i in range(50):
+	layers = []
+	layers.append( choice(dirFiles("layers/body")) )
+	layers.append( choice(dirFiles("layers/cloths")) )
+	layers.append( choice(dirFiles("layers/eyes")) )
+	layers.append( choice(dirFiles("layers/hair")) )
+	compose(layers).save("export/export"+str(i)+".png", "PNG")
